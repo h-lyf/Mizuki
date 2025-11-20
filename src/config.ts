c4a23f32b3b66718852f305a07ab73de83763a42
h-lyf <57041119+h-lyf@users.noreply.github.com>
2025-11-20 19:11:26 +0800

    modified:   src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -20,7 +20,7 @@ import { LinkPreset } from "./types/config";
 const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
-	title: "御剑乘风",
+	title: "御剑乘风の小破站",
 	subtitle: "",
 	siteURL: "https://mizuki.666411.xyz/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-11-19", // 站点开始运行日期，用于站点统计组件计算运行天数
@@ -609,7 +609,7 @@ export const pioConfig: import("./types/config").PioConfig = {
 	mode: "draggable", // 默认为可拖拽模式
 	hiddenOnMobile: true, // 默认在移动设备上隐藏
 	dialog: {
-		welcome: "欢迎来到御剑乘风の博客！", // 欢迎词
+		welcome: "欢迎来到御剑乘风の小破站！", // 欢迎词
 		touch: [
 			"你在干什么？",
 			"再摸我就报警了！",
